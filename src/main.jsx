4c4aa554f30c9ca44eca78dc01fb007fbdf77713
Richard Suarez <richardmsc24@gmail.com>
2023-03-03 11:52:17 -0400

    TEAM-7 Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import App from './App'
-
-=======
 import { RouterProvider } from 'react-router-dom'
->>>>>>> develop
-import './index.css'
+
 import Router from './routes/Route.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
